cf36caccbae8c8871ebca5a406235dc2c5d42ca1
aimxu <aimxu@qq.com>
2020-03-17 18:04:56 +0800

    add multiplation

--- hello.c
+++ hello.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
+#include"add.h"
+#include"mul.h"
 main
 {
 	printf("hello world\n");
 	printf("1+1=%d\n",add(1,1));
+	printf("1-1=%d\n",dec(1,1));
+	printf("1*1=%d\n",mul(1,1));
 	return 0;
 }
